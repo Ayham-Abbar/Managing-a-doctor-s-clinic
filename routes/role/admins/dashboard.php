22bdb687edd96f8dcbc77caa38232fa322244201
mohammad-sagher <mohammadssagher@gmail.com>
2025-02-19 01:24:03 +0300

    add route to redirction controller admin profile

--- routes/role/admins/dashboard.php
+++ routes/role/admins/dashboard.php
@@ -2,9 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\Dashboard\DoctorManagementController;
-
+use App\Http\Controllers\Admin\profileController;
 Route::get('/doctor', [DoctorManagementController::class, 'create'])->name('doctor.create');
 Route::get('/doctor/list', [DoctorManagementController::class, 'index'])->name('doctor.index');
 Route::get('/profile', [DoctorManagementController::class, 'profile'])->name('profile');
 Route::post('/doctor', [DoctorManagementController::class, 'store'])->name('doctor.store');
 Route::put('/doctor/{id}', [DoctorManagementController::class, 'update'])->name('doctor.update');
+
+
+
+
+Route::middleware(['admin'])->prefix('admin')->group(function () {
+    Route::get('profile', [profileController::class, 'index'])->name('admin.profile.index');
+    Route::put('profile', [profileController::class, 'update'])->name('admin.profile.update');
+    Route::post('profile/image', [profileController::class, 'updateImage'])->name('admin.profile.updateImage');
+});
