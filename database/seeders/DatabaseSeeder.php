1943212dcf89f8d3148cb6e218a13878a91fdb9e
Ayham-Abbar <ayham.abbar11@gmail.com>
2025-02-18 08:21:15 +0300

    Seeder To All Roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Admin;
+use App\Models\Accountant;
+use App\Models\Doctor;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 class DatabaseSeeder extends Seeder
@@ -23,6 +25,16 @@ class DatabaseSeeder extends Seeder
         Admin::create([
             'name' => 'Admin',
             'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+        ]);
+        Accountant::create([
+            'name' => 'Accountant',
+            'email' => 'accountant@example.com',
+            'password' => Hash::make('password'),
+        ]);
+        Doctor::create([
+            'name' => 'Doctor',
+            'email' => 'doctor@example.com',
             'password' => Hash::make('password'),
         ]);
     }
