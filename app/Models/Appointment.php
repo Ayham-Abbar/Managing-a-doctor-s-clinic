553f3fd1961d861e634b66f4e70dff7f57ae6e69
mohammad-sagher <mohammadssagher@gmail.com>
2025-02-22 02:10:45 +0300

    add fillable and realtion

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -8,4 +8,29 @@ use Illuminate\Database\Eloquent\Model;
 class Appointment extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'doctor_id',
+        'time_slot_id',
+        'status',
+    ];
+  
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function doctor()
+    {
+        return $this->belongsTo(Doctor::class);
+    }
+
+    public function timeSlot()
+    {
+        return $this->belongsTo(TimeSolt::class);
+    }
+
+
+
 }
