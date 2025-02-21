c14e91c88668d606adefc2d87b3d1f0843c9161a
mohammad-sagher <mohammadssagher@gmail.com>
2025-02-22 02:16:07 +0300

    add routing

--- routes/role/doctors/doctor.php
+++ routes/role/doctors/doctor.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\Doctor\AppointmentController;
 use App\Http\Controllers\Doctor\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Doctor\AvailableTimeController;
 use App\Http\Controllers\Doctor\profileController;
 
+use App\Http\Controllers\Doctor\DashboardController;
 Route::middleware(['doctor'])->prefix('doctor')->group(function () {
     Route::get('available-time', [AvailableTimeController::class, 'index'])->name('doctor.available-time.index');
     Route::get('available-time/create', [AvailableTimeController::class, 'create'])->name('doctor.available-time.create');
@@ -19,4 +21,16 @@ Route::middleware(['doctor'])->prefix('doctor')->group(function () {
     Route::get('profile', [profileController::class, 'index'])->name('doctor.profile.index');
     Route::put('profile', [profileController::class, 'update'])->name('doctor.profile.update');
     Route::post('profile/image', [profileController::class, 'updateImage'])->name('doctor.profile.updateImage');
+});
+
+Route::middleware(['doctor'])->prefix('doctor')->group(function () {
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('doctor.dashboard.index');
+
+    Route::get('appointments/complated', [AppointmentController::class, 'allAppointments'])->name('doctor.appointments.all');
+    Route::get('appointments/canceled', [AppointmentController::class, 'getAllcancelAppointment'])->name('doctor.appointments.canceled')
+    ;
+    Route::get('appointments/upcoming', [AppointmentController::class, 'getAllUpcomingAppointment'])->name('doctor.appointments.upcoming');
+    Route::get('appointments/completed', [AppointmentController::class, 'getAllCompletedAppointment'])->name('doctor.appointments.completed');
+    Route::post('/appointment/update-status', [AppointmentController::class, 'updateStatus'])->name('doctor.appointments.updateStatus');
+    Route::delete('/appointment/{id}', [AppointmentController::class, 'destroy'])->name('doctor.appointments.destroy');
 });
